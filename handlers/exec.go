3adf1199c612e7407979aaf709c041b5b6439830
Everett Wilson <everettasherwilson@gmail.com>
2023-11-14 12:40:18 -0600

    Add fuzzy finder for request execution

--- handlers/exec.go
+++ handlers/exec.go
@@ -2,32 +2,83 @@ package handlers
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/EvWilson/sqump/core"
 	"github.com/EvWilson/sqump/handlers/cmder"
+	"github.com/ktr0731/go-fuzzyfinder"
 )
 
 func ExecOperation() *cmder.Op {
 	return cmder.NewOp(
 		"exec",
-		"exec <squmpfile path> <request title>",
-		"Executes the given request",
+		"exec <squmpfile path> <request title>, or none",
+		"Executes the given request, or fuzzy searches for one if no args provided",
 		handleExec,
 	)
 }
 
 func handleExec(args []string) error {
-	if len(args) != 2 {
-		return fmt.Errorf("expected 2 args to `exec`, got: %d", len(args))
+	switch len(args) {
+	case 0:
+		return handleExecFuzzy()
+	case 2:
+		filepath, requestName := args[0], args[1]
+		sqFile, err := core.ReadSqumpfile(filepath)
+		if err != nil {
+			return err
+		}
+		_, err = sqFile.ExecuteRequest(requestName, make(core.LoopChecker))
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		return fmt.Errorf("expected 0 or 2 args to `exec`, got: %d", len(args))
 	}
-	filepath, requestName := args[0], args[1]
-	sqFile, err := core.ReadSqumpfile(filepath)
+}
+
+func handleExecFuzzy() error {
+	options := make([]string, 0)
+
+	conf, err := core.ReadConfig()
 	if err != nil {
 		return err
 	}
-	_, err = sqFile.ExecuteRequest(requestName, make(core.LoopChecker))
+
+	for _, path := range conf.Files {
+		sq, err := core.ReadSqumpfile(path)
+		if err != nil {
+			return err
+		}
+		for _, req := range sq.Requests {
+			options = append(options, fmt.Sprintf("%s.%s", sq.Title, req.Title))
+		}
+	}
+
+	idx, err := fuzzyfinder.Find(
+		options,
+		func(i int) string {
+			return options[i]
+		},
+	)
 	if err != nil {
 		return err
 	}
+
+	option := options[idx]
+	pieces := strings.Split(option, ".")
+	if len(pieces) != 2 {
+		return fmt.Errorf("more than a single '.' found in request identifier: '%s'", option)
+	}
+	sq, err := conf.SqumpfileByTitle(pieces[0])
+	if err != nil {
+		return err
+	}
+	_, err = sq.ExecuteRequest(pieces[1], make(core.LoopChecker))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
